34aea9310c95415adf89bb0a43d43ac95da965c3
Marko Živanović <zivanovic.marko@icloud.com>
2025-04-09 19:15:58 +0200

    WhatWntWrongWithDecoding

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Marko Zivanovic on 9.4.25..
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
