1861f2c70f11e4fa18ff400ecd38f191a4a424e3
Marko Živanović <zivanovic.marko@icloud.com>
2025-04-20 21:49:10 +0200

    Formatted Launch Date

--- Moonshot/Mission.swift
+++ Moonshot/Mission.swift
@@ -20,7 +20,7 @@ struct Mission: Codable, Identifiable {
     }
     
     let id: Int
-    let launchDate: String?
+    let launchDate: Date?
     let crew: [CrewRole]
     let description: String?
     
@@ -30,6 +30,10 @@ struct Mission: Codable, Identifiable {
     
     var image: String {
         "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
     }
     
 }
