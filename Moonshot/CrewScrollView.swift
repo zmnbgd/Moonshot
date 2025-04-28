30ebf8c6f43872c3fa8126dcfcaa8ce8f133ec3d
Marko Živanović <zivanovic.marko@icloud.com>
2025-04-28 14:26:22 +0200

    Challenge2
    Extract one or two pieces of view code into their own new SwiftUI views – the horizontal scroll view in MissionView is a great candidate, but if you followed my styling then you could also move the Rectangle dividers out too. - CrewScrollView

---
+++ Moonshot/CrewScrollView.swift
@@ -0,0 +1,47 @@
+//
+//  CrewScrollView.swift
+//  Moonshot
+//
+//  Created by Marko Zivanovic on 28.4.25..
+//
+
+import SwiftUI
+
+struct CrewScrollView: View {
+    
+    let crew: [MissionView.CrewMember]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(crew, id: \.role) { CrewMember in
+                    NavigationLink {
+                        AstronautView(astronaut: CrewMember.astronaut)
+                    } label: {
+                        HStack {
+                            Image(CrewMember.astronaut.id)
+                                .resizable()
+                                .frame(width: 125, height: 90)
+                                .clipShape(.capsule)
+
+                            VStack(alignment: .leading) {
+                                Text(CrewMember.astronaut.name)
+                                    .foregroundColor(.white.opacity(0.8))
+                                    .font(.headline)
+                                
+                                Text(CrewMember.role)
+                                    .foregroundColor(.white.opacity(0.8))
+                                
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    CrewScrollView()
+}
