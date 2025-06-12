d8a59cd5994bef6f58df386090db2b12157f25ec
Marko Živanović <zivanovic.marko@icloud.com>
2025-06-12 21:25:46 +0200

    Project 9. - Challenge 3
    / MARK: - Project 9. - Challenge 3. Return to project 8 (Moonshot), and upgrade it to use NavigationLink(value:). This means adding Hashable conformance, and thinking carefully how to use navigationDestination()

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -21,11 +21,40 @@ struct ContentView: View {
             ScrollView {
                 LazyVGrid(columns: columns) {
                     ForEach(missions) { mission in
-                        NavigationLink {
-                            
-                            //Text("Detail view")
-                            MissionView(mission: mission, astronauts: astronauts)
-                        } label: {
+//                        NavigationLink {
+//                            
+//                            //Text("Detail view")
+//                            MissionView(mission: mission, astronauts: astronauts)
+//                        } label: {
+//                            VStack {
+//                                Image(mission.image)
+//                                    .resizable()
+//                                    .scaledToFit()
+//                                    .frame(width: 100, height: 100)
+//                                    .padding()
+//                                
+//                                VStack {
+//                                    Text(mission.displayName)
+//                                        .font(.headline)
+//                                        .foregroundStyle(.white)
+//                                    
+//                                    Text(mission.formattedLaunchDate)
+//                                        .font(.caption)
+//                                        .foregroundStyle(.secondary)
+//                                }
+//                                .padding(.vertical)
+//                                .frame(maxWidth: .infinity)
+//                                .background(.lightBackground)
+//                            }
+//                            .clipShape(.rect(cornerRadius: 10))
+//                            .overlay(
+//                                RoundedRectangle(cornerRadius: 10)
+//                                    .stroke(.lightBackground)
+//                            )
+//                        }
+                        
+                        // MARK: - Project 9. - Challenge 3. Return to project 8 (Moonshot), and upgrade it to use NavigationLink(value:). This means adding Hashable conformance, and thinking carefully how to use navigationDestination()
+                        NavigationLink(value: mission) {
                             VStack {
                                 Image(mission.image)
                                     .resizable()
@@ -52,6 +81,7 @@ struct ContentView: View {
                                     .stroke(.lightBackground)
                             )
                         }
+                        
                     }
                 }
                 .padding([.horizontal, .bottom])
@@ -59,7 +89,12 @@ struct ContentView: View {
             .navigationTitle("Moonshot")
             .background(.darkBackground)
             .preferredColorScheme(.dark)
+            // MARK: - Project 9. - Challenge 3. Return to project 8 (Moonshot), and upgrade it to use NavigationLink(value:). This means adding Hashable conformance, and thinking carefully how to use navigationDestination()
+            .navigationDestination(for: Mission.self) { mission in
+                MissionView(mission: mission, astronauts: astronauts)
+            }
         }
+
     }
 }
 
